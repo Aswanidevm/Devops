c8674a355c872e2cf951d1ed6dabc45ee624de5f
Aswanidevm <maswanidev@gmail.com>
2023-05-31 08:23:36 +0530

    sed

--- roboshop-shell/mongod.sh
+++ roboshop-shell/mongod.sh
@@ -1,5 +1,5 @@
 yum install mongodb-org -y 
 systemctl enable mongod 
 systemctl start mongod 
-sed -e '127.0.0.1 to 0.0.0.0' /etc/mongod.conf
+sed -e 's/127.0.0.1 / 0.0.0.0 /' /etc/mongod.conf
 systemctl restart mongod
