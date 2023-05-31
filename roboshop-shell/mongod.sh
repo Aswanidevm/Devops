c02bd5df256f32e5fd1c7114ff8635a5257a6a82
Aswanidevm <maswanidev@gmail.com>
2023-05-31 08:37:37 +0530

    echo

--- roboshop-shell/mongod.sh
+++ roboshop-shell/mongod.sh
@@ -1,6 +1,10 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y 
-systemctl enable mongod 
-systemctl start mongod 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl restart mongod
+echo "coping repo files"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
+echo "Installing mongodb"
+yum install mongodb-org -y >> /tmp/roboshop.log
+echo "enabling Mongodb"
+systemctl enable mongod >> /tmp/roboshop.log
+systemctl start mongod >> /tmp/roboshop.log
+echo " Updating Listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf >> /tmp/roboshop.log
+systemctl restart mongod >> /tmp/roboshop.log
