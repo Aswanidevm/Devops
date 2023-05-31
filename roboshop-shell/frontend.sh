372e3c6b74f6c0843f136b7b5aaaf5aead3881fd
Aswanidevm <maswanidev@gmail.com>
2023-05-31 08:12:50 +0530

    echo

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -5,10 +5,10 @@ systemctl enable nginx &>> /tmp/roboshop.log
 systemctl start nginx &>> /tmp/roboshop.log
 echo "removing files"
 rm -rf /usr/share/nginx/html/* &>> /tmp/roboshop.log
-echo "Downloding frontend content"&>> /tmp/roboshop.log
+echo "Downloding frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/roboshop.log
-echo "exctacting frontend content"&>> /tmp/roboshop.log
+echo "exctacting frontend content"
 cd /usr/share/nginx/html &>> /tmp/roboshop.log
 unzip /tmp/frontend.zip &>> /tmp/roboshop.log
-echo "restart nginx"&>> /tmp/roboshop.log
+echo "restart nginx"
 systemctl restart nginx &>> /tmp/roboshop.log
