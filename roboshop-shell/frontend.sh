d2e70d0630bca14929c1f7b81f0e59f0a9e2b570
Aswanidevm <maswanidev@gmail.com>
2023-05-31 07:42:02 +0530

    folder

---
+++ roboshop-shell/frontend.sh
@@ -0,0 +1,13 @@
+echo "Install Nginx"
+yum install nginx -y &>> /tmp/roboshop.log
+echo "Enabling Nginx"
+systemctl enable nginx &>> /tmp/roboshop.log
+systemctl start nginx &>> /tmp/roboshop.log
+echo "removing files"
+rm -rf /usr/share/nginx/html/* &>> /tmp/roboshop.log
+echo "Downloding frontend content"&>> /tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/roboshop.log
+echo "exctacting frontend content"&>> /tmp/roboshop.log
+cd /usr/share/nginx/html &>> /tmp/roboshop.log
+unzip /tmp/frontend.zip &>> /tmp/roboshop.log
+systemctl restart nginx &>> /tmp/roboshop.log
