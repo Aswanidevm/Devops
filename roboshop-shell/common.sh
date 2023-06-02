0b21b329b928654b8857417b29710b6e1caf52a7
Aswanidevm <maswanidev@gmail.com>
2023-06-02 08:47:15 +0530

    config folder

--- roboshop-shell/common.sh
+++ roboshop-shell/common.sh
@@ -3,6 +3,7 @@ nocolor="\e[0m"
 path= "/app"
 logfile= "/tmp/roboshop.log"
 user_id= $(id -u)
+code_dir=$(pwd)
 
 if[ $user_id -ne 0 ]; then
 echo Script should be running with sudo
@@ -24,53 +25,78 @@ app_prereq{
 echo -e "${color} Add User ${nocolor}"
 id roboshop &>> $logfile
 if [$? eq 1]; then
-useradd roboshop &>> $logfile
+useradd roboshop &>> ${logfile}
 fi
 status_check $?
 
-rm -rf path &>> $logfile
-mkdir path  &>> $logfile
+rm -rf path &>> ${logfile}
+status_check $?
+mkdir path  &>> ${logfile}
+status_check $?
 
 
-curl -L -o /tmp/component.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip  &>> $logfile
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip  &>> ${logfile}
+status_check $?
 
-cd path  &>> $logfile
+cd path  &>> ${logfile}
+status_check $?
 
-unzip /tmp/component.zip &>> $logfile
+unzip /tmp/c${component}.zip &>> ${logfile}
+status_check $?
 
 
 }
 
 
 systemd(){
-    systemctl daemon-reload &>> $logfile
+  cp ${code_dir}/config/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+    status_check $?
 
-    systemctl enable component  &>> $logfile
+    systemctl daemon-reload &>> ${logfile}
+    status_check $?
 
-    systemctl start component &>> $logfile
+    systemctl enable component  &>> ${logfile}
+    status_check $?
+
+    systemctl start component &>> ${logfile}
+    status_check $?
 }
 
-mongod_schema(){
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $logfile
+schema_setup(){
+  if[ ${schema_type}=mongo];then
+cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>> ${logfile}
+status_check $?
 
-yum install mongodb-org-shell -y &>> $logfile
+yum install mongodb-org-shell -y &>> ${logfile}
+status_check $?
 
-mongo --host MONGODB-SERVER-IPADDRESS <path/schema/component.js &>> $logfile
+mongo --host MONGODB-SERVER-IPADDRESS <path/schema/component.js &>> ${logfile}
+status_check $?
+
+elif[ ${schema_type}=mysql];then
+
+
+
+fi
 }
 
 nodejs() {
 echo -e "${color} Setup nodejs repo ${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $logfile
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${logfile}
+status_check $?
+
 echo -e "${color} Install nodejs ${nocolor}"
-yum install nodejs -y &>> $logfile
+yum install nodejs -y &>> ${logfile}
+status_check $?
 
 app_prereq
 
-npm install  &>> $logfile
+npm install  &>> ${logfile}
+status_check $?
 
 systemd
 
-mongod_schema
+schema_setup
 }
 
 
