e86b25b69bc766861c03316a9c40a1877d905e5c
Aswanidevm <maswanidev@gmail.com>
2023-06-02 08:59:26 +0530

    common

--- roboshop-shell/common.sh
+++ roboshop-shell/common.sh
@@ -21,82 +21,89 @@ status_check()
 }
 
 
-app_prereq{
-echo -e "${color} Add User ${nocolor}"
-id roboshop &>> $logfile
-if [$? eq 1]; then
-useradd roboshop &>> ${logfile}
-fi
-status_check $?
+app_prereq(){
+ echo -e "${color} Add User ${nocolor}"
+  id roboshop &>> $logfile
+  if [$? eq 1]; then
+    useradd roboshop &>> ${logfile}
+  fi
+ status_check $?
 
-rm -rf path &>> ${logfile}
-status_check $?
-mkdir path  &>> ${logfile}
-status_check $?
+ echo -e "${color} remove directory ${nocolor}"
+  rm -rf path &>> ${logfile}
+  status_check $?
 
+ echo -e "${color} add directory ${nocolor}"
+  mkdir path  &>> ${logfile}
+  status_check $?
 
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip  &>> ${logfile}
-status_check $?
+ echo -e "${color} downloading content ${nocolor}"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip  &>> ${logfile}
+  status_check $?
 
-cd path  &>> ${logfile}
-status_check $?
+ echo -e "${color} open directory ${nocolor}"
+  cd path  &>> ${logfile}
+  status_check $?
 
-unzip /tmp/c${component}.zip &>> ${logfile}
-status_check $?
-
-
+ echo -e "${color} unzip content ${nocolor}"
+  unzip /tmp/c${component}.zip &>> ${logfile}
+  status_check $?
 }
 
 
 systemd(){
-  cp ${code_dir}/config/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  echo -e "${color} copying ${component}.service file ${nocolor}"
+    cp ${code_dir}/config/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
     status_check $?
 
+  echo -e "${color} reloading ${component}.service ${nocolor}"
     systemctl daemon-reload &>> ${logfile}
     status_check $?
-
-    systemctl enable component  &>> ${logfile}
+  
+  echo -e "${color} enabling ${component}.service ${nocolor}"
+    systemctl enable ${component}  &>> ${logfile}
     status_check $?
-
-    systemctl start component &>> ${logfile}
+  
+  echo -e "${color} start ${component}.service ${nocolor}"
+    systemctl start ${component} &>> ${logfile}
     status_check $?
 }
 
 schema_setup(){
   if[ ${schema_type}=mongo];then
-cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>> ${logfile}
-status_check $?
+   cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>> ${logfile}
+   status_check $?
 
-yum install mongodb-org-shell -y &>> ${logfile}
-status_check $?
+   yum install mongodb-org-shell -y &>> ${logfile}
+   status_check $?
 
-mongo --host MONGODB-SERVER-IPADDRESS <path/schema/component.js &>> ${logfile}
-status_check $?
+   mongo --host MONGODB-SERVER-IPADDRESS <path/schema/component.js &>> ${logfile}
+   status_check $?
 
-elif[ ${schema_type}=mysql];then
+  elif[ ${schema_type}=mysql];then
 
 
 
-fi
+  fi
 }
 
 nodejs() {
-echo -e "${color} Setup nodejs repo ${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${logfile}
-status_check $?
+  echo -e "${color} Setup nodejs repo ${nocolor}"
+     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${logfile}
+     status_check $?
 
-echo -e "${color} Install nodejs ${nocolor}"
-yum install nodejs -y &>> ${logfile}
-status_check $?
+  echo -e "${color} Install nodejs ${nocolor}"
+     yum install nodejs -y &>> ${logfile}
+     status_check $?
 
-app_prereq
+  app_prereq
 
-npm install  &>> ${logfile}
-status_check $?
+   npm install  &>> ${logfile}
+   status_check $?
 
-systemd
+  systemd
 
-schema_setup
+  schema_setup
 }
 
 
