5a5c89c93cce300ac858557c0ec25f0e214bf6c2
Aswanidevm <maswanidev@gmail.com>
2023-06-04 14:45:47 +0530

    dg

--- roboshop-shell/common.sh
+++ roboshop-shell/common.sh
@@ -1,8 +1,8 @@
 color="\e[31m"
 nocolor="\e[0m"
 code_dir=$(pwd)
-path= "${code_dir}/app"
-log_file= "${code_dir}/tmp/roboshop.log"
+path=${code_dir}/app
+log_file=${code_dir}/tmp/roboshop.log
 rm -f ${log_file}
 user_id= $(id -u)
 if [ "${user_id}" -ne 0 ]; then
