d635a7daafa0f204ffd8f5569595185986f45f39
Aswanidevm <maswanidev@gmail.com>
2023-06-06 17:26:45 +0530

    redis

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -1,11 +1,21 @@
-echo -e "\e[31m Install redis repo \e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> /tmp/roboshop.log
-echo -e "\e[31m Enable redis 6.2 \e[0m"
-yum module enable redis:remi-6.2 -y &>> /tmp/roboshop.log
-echo -e "\e[31m install redis 6.2 \e[0m"
-yum install redis -y  &>> /tmp/roboshop.log
-echo -e "\e[31m change listen address \e[0m"
-sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf &>> /tmp/roboshop.log
-echo -e "\e[31m Enable redis \e[0m"
-systemctl enable redis &>> /tmp/roboshop.log
-systemctl start redis &>> /tmp/roboshop.log
+source common.sh
+
+echo -e "${color} Install redis repo ${nocolor}"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> ${log_file}
+status_check $?
+
+echo -e "${color} Enable redis 6.2 ${nocolor}"
+yum module enable redis:remi-6.2 -y &>> ${log_file}
+status_check $?
+
+echo -e "${color} install redis 6.2 ${nocolor}"
+yum install redis -y  &>> ${log_file}
+status_check $?
+
+echo -e "${color} change listen address ${nocolor}"
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf &>> ${log_file}
+status_check $?
+
+echo -e "${color} Enable redis ${nocolor}"
+systemctl enable redis &>> ${log_file}
+systemctl start redis &>> ${log_file}
