06a77b649b00455cd43e479833ec105ecaf103f3
Aswanidevm <maswanidev@gmail.com>
2023-06-06 08:39:46 +0530

    -e

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -8,22 +8,22 @@ echo -e "${color} Removing files ${nocolor}"
 rm -rf /usr/share/nginx/html/* &>> /tmp/roboshop.log
 status_check $?
 
-echo "${color} Downloding frontend content ${nocolor}"
+echo -e "${color} Downloding frontend content ${nocolor}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/roboshop.log
 status_check $?
 
-echo "${color} Exctacting frontend content ${nocolor}"
+echo -e "${color} Exctacting frontend content ${nocolor}"
 cd /usr/share/nginx/html &>> /tmp/roboshop.log
 status_check $?
 
 unzip /tmp/frontend.zip &>> /tmp/roboshop.log
 status_check $?
 
-echo "${color} Enabling Nginx ${nocolor}"
+echo -e "${color} Enabling Nginx ${nocolor}"
 systemctl enable nginx &>> /tmp/roboshop.log
 systemctl start nginx &>> /tmp/roboshop.log
 status_check $?
 
-echo "${color} Restart nginx ${nocolor}"
+echo -e "${color} Restart nginx ${nocolor}"
 systemctl restart nginx &>> /tmp/roboshop.log
 status_check $?
