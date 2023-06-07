efd1e919537802194c4c3eb07b9b636fea38bf42
Aswanidevm <maswanidev@gmail.com>
2023-06-07 07:57:52 +0530

    dispaatch

--- roboshop-shell/dispatch.sh
+++ roboshop-shell/dispatch.sh
@@ -1,13 +1,11 @@
-yum install golang -y
-useradd roboshop
-mkdir /app 
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip 
-cd /app 
-unzip /tmp/dispatch.zip
-cd /app 
-go mod init dispatch
-go get 
-go build
-systemctl daemon-reload
-systemctl enable dispatch 
-systemctl start dispatch
+source common.sh
+
+roboshop_app_password=$1
+if [ -z "${roboshop_app_password}" ]; then
+  echo -e "\e[31mMissing RabbitMQ App User Password argument.\e[0m"
+  exit 1
+fi
+
+component=dispatch
+
+golang
