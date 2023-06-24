72a31f118f7421e26cf871f6b7e898bfb427ffe5
Aswanidevm <maswanidev@gmail.com>
2023-06-24 07:56:15 +0530

    terra

--- roboshop-terra/roboshop-infra/ec2/main.tf
+++ roboshop-terra/roboshop-infra/ec2/main.tf
@@ -9,3 +9,10 @@ resource "aws_instance" "example" {
     Name = "${name}"
   }
 }
+
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["self"]
+}
